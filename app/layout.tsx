cd759ec8fca2f172d818108803f1f33ad4915f53
ryouma <junjieduan8@gmail.com>
2026-07-14 20:53:08 +0800

    fix

--- app/layout.tsx
+++
@@ -1,3 +0,0 @@
-import './styles.css';
-export const metadata={title:'轻量级在线客服系统',description:'Web Customer Support System'};
-export default function RootLayout({children}:{children:React.ReactNode}){return <html lang="zh-CN"><body>{children}</body></html>}
