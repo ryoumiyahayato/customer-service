910f4baae7b364a3ac89167c0138ca5261869c30
ryouma <junjieduan8@gmail.com>
2026-08-07 09:15:38 -0700

    feat: mount super admin staff clear control

--- src/apps/AdminApp.tsx
+++ src/apps/AdminApp.tsx
@@ -1,5 +1,11 @@
 import AdminDashboard from '../admin/AdminDashboard';
+import SuperAdminStaffClearControl from '../admin/SuperAdminStaffClearControl';
 
 export default function AdminApp() {
-  return <AdminDashboard />;
+  return (
+    <>
+      <AdminDashboard />
+      <SuperAdminStaffClearControl />
+    </>
+  );
 }
