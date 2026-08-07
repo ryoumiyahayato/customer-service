5011d5e69af72cb77a918fec179e675217204487
ryouma <junjieduan8@gmail.com>
2026-08-07 09:31:01 -0700

    style: load mobile admin polish

--- src/apps/AdminApp.tsx
+++ src/apps/AdminApp.tsx
@@ -1,5 +1,6 @@
 import AdminDashboard from '../admin/AdminDashboard';
 import SuperAdminStaffClearControl from '../admin/SuperAdminStaffClearControl';
+import '../admin/mobileAdminPolish.css';
 
 export default function AdminApp() {
   return (
