accbd3d8af2b55b6ebeb722941bd52fa74748302
ryouma <junjieduan8@gmail.com>
2026-08-08 09:42:35 -0700

    refactor: make dashboard the single admin root

--- src/apps/AdminApp.tsx
+++ src/apps/AdminApp.tsx
@@ -1,22 +1,9 @@
 import AdminDashboard from '../admin/AdminDashboard';
-import DesktopAdminPolish from '../admin/DesktopAdminPolish';
-import SuperAdminStaffClearControl from '../admin/SuperAdminStaffClearControl';
-import AdminMobileShell from '../admin/AdminMobileShell';
-import SessionClientInfo from '../admin/SessionClientInfo';
-import AdminUnreadBadge from '../admin/AdminUnreadBadge';
 import '../admin/mobileAdminPolish.css';
 import '../admin/adminShellFinal.css';
 import '../admin/adminRegressionFixes.css';
+import '../admin/adminUnreadBadge.css';
 
 export default function AdminApp() {
-  return (
-    <>
-      <AdminDashboard />
-      <DesktopAdminPolish />
-      <SessionClientInfo />
-      <AdminMobileShell />
-      <SuperAdminStaffClearControl />
-      <AdminUnreadBadge />
-    </>
-  );
+  return <AdminDashboard />;
 }
